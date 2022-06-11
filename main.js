6ae6310dc8a65f77b0c2f276ca3a7d866edf9418
Amir Hossain <amirhossain.cse113@gmail.com>
2022-06-11 19:37:31 +0600

    updated

--- main.js
+++ main.js
@@ -32,3 +32,22 @@ navItems.forEach(item => {
         link.classList.add('active');
     })
 });
+
+// project popup 
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('work_button')){
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector('.portfolio_popup').classList.toggle('open');
+}
+
+document.querySelector('.portfolio_popup-close').addEventListener('click', togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector('.pp_thumbnail img').src = portfolioItem.querySelector('.work_img').src;
+    document.querySelector('.portfolio_popup-body').innerHTML = portfolioItem.querySelector('.portfolio_item-details').innerHTML;
+}
